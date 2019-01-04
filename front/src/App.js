e9720446af6047521fc8753b85f034efee9b7968
Ayoub Ochan <ayoub.ochan13@gmail.com>
2019-01-04 14:59:33 +0100

    installation of Eslint

--- front/src/App.js
+++ front/src/App.js
@@ -1,16 +1,10 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          
-        </header>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="App">
+    <header className="App-header" />
+  </div>
+);
 
 export default App;
