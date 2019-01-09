9f3bdbeea112bb09e21e61dfc8411a8bf1e99075
Ayoub Ochan <ayoub.ochan13@gmail.com>
2019-01-09 14:41:08 +0100

    Barre des étapes pour calendrier

--- front/src/components/Simulator/index.js
+++ front/src/components/Simulator/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './index.css';
 import LeftSide from '../LeftSide/index';
+import StepBar from '../StepBar/index';
 
 class Simulator extends Component {
   constructor(props) {
@@ -13,6 +14,7 @@ class Simulator extends Component {
   render() {
     return (
       <div>
+        <StepBar />
         <LeftSide />
       </div>
     );
---
+++ front/src/components/StepBar/index.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './index.css';
+
+class StepBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+      </ul>
+    );
+  }
+}
+
+export default StepBar;
