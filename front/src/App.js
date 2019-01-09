e5a4598e8afbfb24ee3683116286a03bebe1260a
Cristina Gonzalez <cgonzalezescallon@gmail.com>
2019-01-09 13:49:54 +0100

    pg concept sans scss

--- front/src/App.js
+++ front/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import Concept from './components/Concept/index';
 
 const App = () => (
   <div className="App">
     <header className="App-header" />
+    <Concept />
   </div>
 );
 
---
+++ front/src/components/Concept/index.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+
+class Concept extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <section className="section_concept1">
+          <div className="container">
+            <div className="container_img_all">
+              <div className="container_title">
+                <h2>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet</h2>
+                <div className="container_text">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                  <p>Curabitur eget arcu porttitor, hendrerit magna semper, facilisis elit.</p>
+                  <p>Fusce vitae posuere purus, a porta diam. Nunc volutpat, sem nec porttitor.</p>
+                  <p>In ornare sem ac neque ultricies aliquet. Phasellus sit amet sem congue.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="section_concept2">
+          <div className="container">
+            <div className="container_img_all">
+              <div className="container_title">
+                <h2>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet</h2>
+                <div className="container_text">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                  <p>Curabitur eget arcu porttitor, hendrerit magna semper, facilisis elit.</p>
+                  <p>Fusce vitae posuere purus, a porta diam. Nunc volutpat, sem nec porttitor.</p>
+                  <p>In ornare sem ac neque ultricies aliquet. Phasellus sit amet sem congue.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="section_concept3">
+          <div className="container">
+            <div className="container_img_all">
+              <div className="container_title">
+                <h2>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet</h2>
+                <div className="container_text">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                  <p>Curabitur eget arcu porttitor, hendrerit magna semper, facilisis elit.</p>
+                  <p>Fusce vitae posuere purus, a porta diam. Nunc volutpat, sem nec porttitor.</p>
+                  <p>In ornare sem ac neque ultricies aliquet. Phasellus sit amet sem congue.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    );
+  }
+}
+
+export default Concept;
