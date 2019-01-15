decfdc07f060de2b71cd34eb89b968bb3bd4ae50
Ayoub Ochan <ayoub.ochan13@gmail.com>
2019-01-15 16:52:11 +0100

    Next button finish

---
+++ front/src/components/NextButton/index.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './index.css';
+import { NavLink } from 'react-router-dom';
+
+class NextButton extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  handleClick() {
+    this.props.next();
+  }
+
+  render() {
+    return (
+      <NavLink to={`/${this.props.current}`}><button type="button" onClick={() => this.handleClick()}>Suivant</button></NavLink>
+    );
+  }
+}
+
+export default NextButton;
--- front/src/components/Simulator/index.js
+++ front/src/components/Simulator/index.js
@@ -3,14 +3,21 @@ import './index.css';
 import { withRouter } from 'react-router';
 import LeftSide from '../LeftSide/index';
 import StepBar from '../StepBar/index';
+import NextButton from '../NextButton/index';
 
 class Simulator extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      current: 1,
     };
   }
 
+  next() {
+    this.setState(prevState => ({
+      current: prevState.current + 1,
+    }));
+  }
 
   render() {
     if (this.props.location.pathname === '/1') {
@@ -18,6 +25,7 @@ class Simulator extends Component {
         <div>
           <StepBar />
           <LeftSide />
+          <NextButton next={() => this.next()} current={this.state.current} />
         </div>
       );
     }
