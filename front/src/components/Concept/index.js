fc97b9b3c3bc391327cdf1c86dd9e791e3e326ef
Cristina Gonzalez <cgonzalezescallon@gmail.com>
2019-01-16 11:31:36 +0100

    scss en cours

--- front/src/components/Concept/index.js
+++ front/src/components/Concept/index.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import './index.scss';
+import img2 from './img2.png';
+// import img1 from './img1.png';
+// import img3 from './img3.png';
 
 class Concept extends Component {
   constructor(props) {
@@ -10,49 +14,41 @@ class Concept extends Component {
   render() {
     return (
       <div>
-        <section className="section_concept1">
-          <div className="container">
-            <div className="container_img_all">
-              <div className="container_title">
-                <h2>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet</h2>
-                <div className="container_text">
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                  <p>Curabitur eget arcu porttitor, hendrerit magna semper, facilisis elit.</p>
-                  <p>Fusce vitae posuere purus, a porta diam. Nunc volutpat, sem nec porttitor.</p>
-                  <p>In ornare sem ac neque ultricies aliquet. Phasellus sit amet sem congue.</p>
-                </div>
-              </div>
-            </div>
+        <section className="section section1">
+          <div className="-section_container">
+            <h2 className="section1_container_title">Notre concept</h2>
+          </div>
+          <div className="section_container_text">
+            <h3>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <p>Curabitur eget arcu porttitor, hendrerit magna semper, facilisis elit.</p>
+            <p>Fusce vitae posuere purus, a porta diam. Nunc volutpat, sem nec porttitor.</p>
+            <p>In ornare sem ac neque ultricies aliquet. Phasellus sit amet sem congue.</p>
           </div>
         </section>
-        <section className="section_concept2">
-          <div className="container">
-            <div className="container_img_all">
-              <div className="container_title">
-                <h2>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet</h2>
-                <div className="container_text">
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                  <p>Curabitur eget arcu porttitor, hendrerit magna semper, facilisis elit.</p>
-                  <p>Fusce vitae posuere purus, a porta diam. Nunc volutpat, sem nec porttitor.</p>
-                  <p>In ornare sem ac neque ultricies aliquet. Phasellus sit amet sem congue.</p>
-                </div>
-              </div>
-            </div>
+
+        <section className="section2">
+          <div className="section_container_text">
+            <h3>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <p>Curabitur eget arcu porttitor, hendrerit magna semper, facilisis elit.</p>
+            <p>Fusce vitae posuere purus, a porta diam. Nunc volutpat, sem nec porttitor.</p>
+            <p>In ornare sem ac neque ultricies aliquet. Phasellus sit amet sem congue.</p>
+          </div>
+          <div className="section_container_place">
+            <p>
+              <img className="section_container_place_image" src={img2} alt="img_section2" />
+            </p>
           </div>
         </section>
-        <section className="section_concept3">
-          <div className="container">
-            <div className="container_img_all">
-              <div className="container_title">
-                <h2>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet</h2>
-                <div className="container_text">
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                  <p>Curabitur eget arcu porttitor, hendrerit magna semper, facilisis elit.</p>
-                  <p>Fusce vitae posuere purus, a porta diam. Nunc volutpat, sem nec porttitor.</p>
-                  <p>In ornare sem ac neque ultricies aliquet. Phasellus sit amet sem congue.</p>
-                </div>
-              </div>
-            </div>
+
+        <section className="section section3">
+          <div className="section_container_text">
+            <h3>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <p>Curabitur eget arcu porttitor, hendrerit magna semper, facilisis elit.</p>
+            <p>Fusce vitae posuere purus, a porta diam. Nunc volutpat, sem nec porttitor.</p>
+            <p>In ornare sem ac neque ultricies aliquet. Phasellus sit amet sem congue.</p>
           </div>
         </section>
       </div>
