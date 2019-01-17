b90778e4e7f9e0f2742c49e326ce192378629cdd
celestinebonaert <celestine.bonaert@gmail.com>
2019-01-17 13:43:01 +0100

    css navBar

--- front/src/App.js
+++ front/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import NavBar from './components/NavBar/index'
+import NavBar from './components/NavBar/index';
 
 const App = () => (
   <div className="App">
--- front/src/components/NavBar/index.js
+++ front/src/components/NavBar/index.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import './index.scss';
+import Logomenu from './logomenu.png';
 
 class NavBar extends Component {
   constructor(props) {
@@ -9,11 +11,18 @@ class NavBar extends Component {
 
   render() {
     return (
-      <nav className="navigation-bar">
-        <ul>
-          <li className="nav-list"><a href="#welcome" nav-section="welcome">Welcome</a></li>
-          <li className="nav-list"><a href="#concept" nav-section="concept">Concept</a></li>
-          <li className="nav-list"><a href="#mon-calendrier" nav-section="mon-calendreir">Mon calendrier</a></li>
+      <nav className="navbar">
+        <ul className="navbar_tableau">
+          <li className="navbar_tableau_welcome">
+            <img className="logomenu" src={Logomenu} alt="fleur logo" />
+          </li>
+          <li className="navbar_tableau_welcome">
+            <a className="navbar_tableau_welcome_link" href="#welcome" alt="welcome" Title="Welcome">WELCOME</a>
+          </li>
+        </ul>
+        <ul className="navbar_tableau">
+          <li className="navbar_tableau_concept"><a href="#calendrier">Calendrier</a></li>
+          <li className="navbar_tableau_concept navbar_tableau_concept--padding"><a href="#concept">Concept</a></li>
         </ul>
       </nav>
     );
