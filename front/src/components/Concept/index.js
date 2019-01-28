17d74860300764cddb7c4cee1ff76b2a7dbe339e
Cristina Gonzalez <cgonzalezescallon@gmail.com>
2019-01-28 09:39:44 +0100

    scss fin

--- front/src/components/Concept/index.js
+++ front/src/components/Concept/index.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 import './index.scss';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import img2 from './img2.png';
-// import img1 from './img1.png';
-// import img3 from './img3.png';
+import img1 from './img1.png';
+import img3 from './img3.png';
+
 
 class Concept extends Component {
   constructor(props) {
@@ -11,46 +14,72 @@ class Concept extends Component {
     };
   }
 
+  componentDidMount() {
+    AOS.init({
+      duration: 2000,
+    });
+  }
+
   render() {
     return (
       <div>
-        <section className="section section1">
-          <h2 className="section1_title">Notre concept</h2>
-          <div className="section_text">
-            <h3 className="section_text_h3">Neque porro quisquam est qui dolorem ipsum quia dolor sit amet</h3>
-            <p className="section_text_p">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Curabitur eget arcu porttitor, hendrerit magna semper, facilisis elit.
-              Fusce vitae posuere purus, a porta diam. Nunc volutpat, sem nec porttitor.
-              In ornare sem ac neque ultricies aliquet. Phasellus sit amet sem congue.
-            </p>
+        <section className="section_container">
+          <div>
+            <h2 className="section_title_1_text section_title_text">1. Lorem ipsum dolor</h2>
+          </div>
+          <div className="container-flex" data-aos="fade-right">
+            <div className="container_text_1">
+              <h3 className="container_text_title">Lorem ipsum dolor</h3>
+              <p className="container_text_text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Duis ex lacus, porta vel dui non, volutpat imperdiet urna.
+                Donec porta euismod nibh vel interdum. Nulla at arcu in ligula ultricies viverra.
+                Nunc nec elit et velr adipiscing elit.
+                Duis ex lacus, porta vel dui non, volutpat imperdiet urna.
+                Donec porta euismod nibh vel interdum. Nulla at arcu in ligula ultricies viverra.
+              </p>
+            </div>
+            <img className="img1" src={img1} alt="imgconcept" data-aos="fade-left" />
           </div>
         </section>
 
-        <section className="section section2">
-          <div className="section_text">
-            <h3 className="section_text_h3">Neque porro quisquam est qui dolorem ipsum quia dolor sit amet</h3>
-            <p className="section_text_p">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Curabitur eget arcu porttitor, hendrerit magna semper, facilisis elit.
-              Fusce vitae posuere purus, a porta diam. Nunc volutpat, sem nec porttitor.
-              In ornare sem ac neque ultricies aliquet. Phasellus sit amet sem congue.
-            </p>
+        <section className="section_container">
+          <div>
+            <h2 className="section_title_2_text section_title_text">2. Lorem ipsum dolor</h2>
           </div>
-          <div className="section2_container">
-            <img className="section2_container_image" src={img2} alt="img_section2" />
+          <div className="container-flex_2" data-aos="fade-left">
+            <div className="container_text_2">
+              <h3 className="container_text_title">Lorem ipsum dolor</h3>
+              <p className="container_text_text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Duis ex lacus, porta vel dui non, volutpat imperdiet urna.
+                Donec porta euismod nibh vel interdum. Nulla at arcu in ligula ultricies viverra.
+                Nunc nec elit et velr adipiscing elit.
+                Duis ex lacus, porta vel dui non, volutpat imperdiet urna.
+                Donec porta euismod nibh vel interdum. Nulla at arcu in ligula ultricies viverra.
+              </p>
+            </div>
+            <img className="img1 img2" src={img2} alt="imgconcept" data-aos="fade-right" />
           </div>
         </section>
 
-        <section className="section section3">
-          <div className="section_text">
-            <h3 className="section_text_h3">Neque porro quisquam est qui dolorem ipsum quia dolor sit amet</h3>
-            <p className="section_text_p">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Curabitur eget arcu porttitor, hendrerit magna semper, facilisis elit.
-              Fusce vitae posuere purus, a porta diam. Nunc volutpat, sem nec porttitor.
-              In ornare sem ac neque ultricies aliquet. Phasellus sit amet sem congue.
-            </p>
+        <section className="section_container">
+          <div>
+            <h2 className="section_title_3_text section_title_text">3. Lorem ipsum dolor</h2>
+          </div>
+          <div className="container-flex" data-aos="fade-right">
+            <div className="container_text_3">
+              <h3 className="container_text_title">Lorem ipsum dolor</h3>
+              <p className="container_text_text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Duis ex lacus, porta vel dui non, volutpat imperdiet urna.
+                Donec porta euismod nibh vel interdum. Nulla at arcu in ligula ultricies viverra.
+                Nunc nec elit et velr adipiscing elit.
+                Duis ex lacus, porta vel dui non, volutpat imperdiet urna.
+                Donec porta euismod nibh vel interdum. Nulla at arcu in ligula ultricies viverra.
+              </p>
+            </div>
+            <img className="img1" src={img3} alt="imgconcept" data-aos="fade-left" />
           </div>
         </section>
       </div>
