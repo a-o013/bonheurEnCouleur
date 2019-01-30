f2336aad168645c3f60ddb500bec0cf344d277fb
Cristina Gonzalez <cgonzalezescallon@gmail.com>
2019-01-30 14:56:47 +0100

    Order section finiched along with the corresponding scss

--- front/src/App.js
+++ front/src/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
-import './App.css';
-import { Route, Switch } from 'react-router-dom';
-import Simulator from './components/Simulator/index';
-import Home from './components/Home/index';
+import './App.scss';
+// import { Route, Switch } from 'react-router-dom';
+// import Simulator from './components/Simulator/index';
+// import Home from './components/Home/index';
+import OrderSection from './components/OrderSection';
 
 const App = () => (
   <div className="App">
-    <Switch>
-      <Route path="/" component={Home} exact />
-      <Route path="/:section/:step" component={Simulator} />
-    </Switch>
+    <OrderSection />
   </div>
 );
 
 export default App;
+
+/* <div className="App">
+    <Switch>
+      <Route path="/" component={Home} exact />
+      <Route path="/:section/:step" component={Simulator} />
+    </Switch>
+  </div> */
--- front/src/components/OrderSection/index.js
+++ front/src/components/OrderSection/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import './index.scss';
+import './indexx.scss';
 import { NavLink } from 'react-router-dom';
+import calendrier from '../../assets/images/calendrier.png';
+import recharge from '../../assets/images/recharge.png';
 
 class OrderSection extends Component {
   constructor(props) {
@@ -11,12 +13,23 @@ class OrderSection extends Component {
 
   render() {
     return (
-      <div>
-        <div>
-          <NavLink to="/calendrier/1">Calendrier</NavLink>
+      <div className="card">
+        <div className=" card_holder">
+          <NavLink to="/calendrier/1" className="card_holder_text">
+            <div className="card_holder_border">
+              Calendrier
+              <img src={calendrier} alt="icone calendrier" className="icone" />
+            </div>
+          </NavLink>
         </div>
-        <div>
-          <NavLink to="/recharge/1">Recharge</NavLink>
+
+        <div className=" card_holder">
+          <NavLink to="/recharge/1" className="card_holder_text">
+            <div className="card_holder_border">
+              Recharge
+              <img src={recharge} alt="icone recharge" className="icone" />
+            </div>
+          </NavLink>
         </div>
       </div>
     );
