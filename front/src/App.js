cd8b1033a6889898230118e21725a575d65b3d8d
Cristina Gonzalez <cgonzalezescallon@gmail.com>
2019-01-30 14:57:42 +0100

    Order section finiched along with the corresponding scss

--- front/src/App.js
+++ front/src/App.js
@@ -1,21 +1,16 @@
 import React from 'react';
 import './App.scss';
-// import { Route, Switch } from 'react-router-dom';
-// import Simulator from './components/Simulator/index';
-// import Home from './components/Home/index';
-import OrderSection from './components/OrderSection';
+import { Route, Switch } from 'react-router-dom';
+import Simulator from './components/Simulator/index';
+import Home from './components/Home/index';
 
 const App = () => (
   <div className="App">
-    <OrderSection />
-  </div>
-);
-
-export default App;
-
-/* <div className="App">
     <Switch>
       <Route path="/" component={Home} exact />
       <Route path="/:section/:step" component={Simulator} />
     </Switch>
-  </div> */
+  </div>
+);
+
+export default App;
