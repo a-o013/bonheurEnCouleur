b79ee34e5b156ce1c6a0d6808ef4c754f7ece94c
Cristina Gonzalez <cgonzalezescallon@gmail.com>
2019-02-04 18:04:24 +0100

    modif

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.scss';
-import ListeCouleurs from './components/ListeCouleurs/index';
+import { Switch, Route } from 'react-router-dom';
+import Home from './components/Home/index';
+import Simulator from './components/Simulator/index';
 
 const App = () => (
   <div className="App">
     <header className="App-header" />
-    <ListeCouleurs />
+    <Switch>
+      <Route path="/" component={Home} exact />
+      <Route path="/:section/:step" component={Simulator} />
+    </Switch>
   </div>
 );
 
