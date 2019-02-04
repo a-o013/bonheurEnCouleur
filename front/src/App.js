b526fc4aa9c185bcdf3a2d9038a2d82bfac38c3f
Ayoub Ochan <ayoub.ochan13@gmail.com>
2019-02-04 18:22:19 +0100

    correction

--- front/src/App.js
+++ front/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import {Switch, Route} from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import Home from './components/Home/index';
 import Simulator from './components/Simulator/index';
 
