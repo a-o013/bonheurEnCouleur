925651d82bd21dec45114d0f7a4448a30f60f739
celestinebonaert <celestine.bonaert@gmail.com>
2019-02-15 15:40:11 +0100

    finition finale footer

--- front/src/components/Footer/index.js
+++ front/src/components/Footer/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './index.scss';
+import logofacebook from '../../assets/images/logofacebook.png';
+import logowcs from '../../assets/images/logowcs.png';
 
 class Footer extends Component {
   constructor(props) {
@@ -12,8 +14,19 @@ class Footer extends Component {
     return (
       <footer className="footer">
         <div className="footer__link">
+
           <img className="footer__link__image" src="https://image.noelshack.com/fichiers/2019/06/2/1549385979-logo-fleur.png" alt="fleur logo" />
-          <p className="footer__link__bec">Le bonheur en couleurs</p>
+          <a href="https://www.lerobert.com/sites/default/files/common/CGU.pdf"><p className="footer__link__text">Conditions d’utilisation</p></a>
+        </div>
+        <div className="footer__facebook">
+          <p className="footer__facebook__text">Suivez-moi sur Facebook</p>
+          <a href="https://www.facebook.com/bonheurencouleurs/">
+            <img className="footer__facebook__image" src={logofacebook} alt="logo facebook" />
+          </a>
+        </div>
+        <div className="footer__wcs">
+          <p className="footer__wcs__text">© 2019 - Made with ♡ by  </p>
+          <a href="https://wildcodeschool.be/"><img className="footer__wcs__image" src={logowcs} alt="logo wild code school" /></a>
         </div>
       </footer>
     );
