c6db7809b249928b29215f8f742ce59fab74482e
Ayoub Ochan <ayoub.ochan13@gmail.com>
2019-02-25 11:27:07 +0100

    delet img

--- front/src/components/FirstPage/index.js
+++ front/src/components/FirstPage/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './index.scss';
 import logo1 from '../../assets/images/logo_fleur.png';
-import logo2 from '../../assets/images/logo_calendrier.png';
 
 class FirstPage extends Component {
   constructor(props) {
@@ -19,9 +18,6 @@ class FirstPage extends Component {
             <img src={logo1} alt="fleur" className="background__text__image" />
             <p className="background__text__p">100 % personnalisable</p>
           </div>
-        </div>
-        <div className="container2">
-          <img src={logo2} alt="calendrier" className="container2__image" />
         </div>
       </div>
     );
